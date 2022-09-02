4373a47dba0c3bb308f25ee40370fcbeeba1e1e9
Monster0506 <traklovits@gmail.com>
2022-09-02 15:32:20 -0400

    wip

--- certificate.py
+++ certificate.py
@@ -1,13 +1,14 @@
+import csv
 import os
-from re import S
 from typing import List
 
 from Crypto.PublicKey import RSA
 
 import KeyTrust
-from main import generate_key_pair, sign
+from keys import _import_key
+from main import generate_key_pair, public_key, sign
 from main import verify as vfy
-from utils import PRINT, json, toStr, write_json
+from utils import PRINT, json, toBytes, toStr, write_json
 
 
 def has_sufficient_trust(keys: List[KeyTrust.KeyTrust], level: int = 5):
@@ -31,7 +32,7 @@ def has_sufficient_trust(keys: List[KeyTrust.KeyTrust], level: int = 5):
 
 
 class Credentials(dict):
-    def __init__(self, id, **kwargs):
+    def __init__(self, id,  **kwargs):
         super().__init__(id=id, **kwargs)
 
     def __dict__(self):
@@ -54,15 +55,15 @@ class Certificate(dict):
         hash_alg = hash_alg.upper()
         credentials = json.loads(credentials.__dict__())
         signature = str(signature)
-        super().__init__(
-            public_key=public_key,
-            key_alg=key_alg,
-            hash_alg=hash_alg,
-            enc_alg=enc_alg,
-            credentials=credentials,
-            signature=signature
+        self.__dict__ = {
+            "public_key": public_key,
+            "key_alg": key_alg,
+            "hash_alg": hash_alg,
+            "enc_alg": enc_alg,
+            "credentials": credentials,
+            "signature": signature
 
-        )
+        }
 
     def sign(self, private_key: RSA.RsaKey):
         return sign(str(self.copy()), private_key)
@@ -70,17 +71,24 @@ class Certificate(dict):
     def verify(self, publicKey: RSA.RsaKey, digest):
         return vfy(str(self.copy()), digest, publicKey)
 
+    def __str__(self) -> str:
+        return str(self.__dict__)
+
     def get(self, name):
         names = ["KEY_ALG", "HASH_ALG", "ENC_ALG",
                  "PUBLIC_KEY", "CREDENTIALS", "SIGNATURE"]
         if name.upper() in names:
-            return (self[name.lower()])
+            return (self.__dict__[name.lower()])
         else:
             raise KeyError("Invalid key")
 
-    def store(self):
+    def store(self, filename):
         # print(self)
-        return json.loads(json.dumps(self))
+        with open(filename, "a") as f:
+            writer = csv.DictWriter(f, fieldnames=self.__dict__.keys())
+            if os.stat(filename).st_size == 0:
+                writer.writeheader()
+            writer.writerow(self.__dict__)
 
     @ staticmethod
     def read(file):
@@ -92,33 +100,37 @@ class Certificate(dict):
         Returns:
             Certificate: The new Certificate object.
         """
-        with open(file, "r") as f:
-            decoded = json.loads((f.read()).replace("'", '"'))
-            public_key = RSA.importKey((decoded.get("public_key")))
-            credentials = Credentials(**decoded.get("credentials"))
+        with open(file, 'r') as f:
+            reader = csv.DictReader(f)
+            line_num = 0
+            for row in reader:
+                if line_num == 0:
+                    line_num += 1
+                public_key = _import_key(toBytes(row["public_key"]))
+                key_alg = row["key_alg"]
+                hash_alg = row["hash_alg"]
+                enc_alg = row["enc_alg"]
+                signature = row["signature"]
+                credentials = row["credentials"]
 
-            digest = (decoded.get("digest")).decode(
-                'utf-8') if "digest" in decoded else None
+                creds = Credentials(credentials)
+                yield Certificate(public_key, creds, key_alg, hash_alg, enc_alg, signature)
 
-            yield Certificate(public_key, credentials, (decoded.get("key_alg")), (decoded.get("hash_alg")), (decoded.get("enc_alg")), signature=digest)
+                line_num += 1
 
 
 if __name__ == "__main__" and PRINT:
     pubkey, priv = generate_key_pair()
-    creds = Credentials("test", username="test", password="test")
+    creds = Credentials(id="test", username="test", password="test")
     certif = Certificate(pubkey, creds)
-    stored = certif.store()
-    write_json(stored, 'certifs.json',)
+    certif.store("certificate.csv")
 
-    certif = list(Certificate.read("certifs.json"))
+    certif = list(Certificate.read("certificate.csv"))
+    keys = ["KEY_ALG", "HASH_ALG", "ENC_ALG",
+            "PUBLIC_KEY", "CREDENTIALS", "SIGNATURE"]
     for cert in certif:
-        # print(cert)
-        print(cert.store())
-        print(cert.get("KEY_ALG"))
-        print(cert.get("HASH_ALG"))
-        print(cert.get("ENC_ALG"))
-        print(cert.get("PUBLIC_KEY"))
-        print(cert.get("CREDENTIALS"))
+        for key in keys:
+            print(f'{key}: {cert.get(key)}')
         signed = cert.sign(priv)
         print(cert.verify(pubkey, signed))
-        print(cert)
+        print(cert.get("SIGNATURE"))
